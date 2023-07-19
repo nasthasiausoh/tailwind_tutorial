a815eeae0ea363ce4edc47fa206b7332cea542b0
Nasthasia Usoh <nasthasia@hotmail.co.uk>
2023-07-19 20:52:41 +0100

    refactoring navbar for better mobile interface

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,11 +36,11 @@ const Navbar = () => {
             {/* Since this is a .jsx file we have been typing in JSX however, if we want to include JavaScript syntax, we must include curly braces and put the text within there like we have done below: */}
             <div onClick={handleNav} className='block md:hidden'> 
             {/* gave this a class name because we dont want it to show unless we are on smaller screens* So we are displaying=block and anything above the medium break point (778px) then qe want it to be hidden. (There are different break points. default is approx 660px and medium is 778px) */ }
-                {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} /> }
+                {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} /> }
                 {/* this means: if the nav is TRUE then show the X menu icon otherwise so the normal hamburger menu icon. These two lines allow us to toggle the icons when we click on it. */}
 
 
-            <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ' : 'fixed left-[-100%]'} >
+            <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ' : 'fixed left-[-100%]'} >
             {/* So if !nav then display vertical navbar if not then hide the nav bar (hence the -100%) */}
 
             <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
